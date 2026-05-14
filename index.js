6c187a653699f45526fdb3bce75414111f53b80d
Euphorxx <97964078+Euphorxx@users.noreply.github.com>
2026-05-14 18:02:31 +0000

    feat: add function to get computer choice

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const rock = 'Rock';
 const paper = 'Paper';
 const scissors = 'Scissors';
 
+function getComputerChoice()  {
+    const random = Math.floor(Math.random() * 3) + 1;
+    if (random === 1) {return (rock);
+    } else if (random === 2) {return (paper);
+    } else if (random === 3) {return (scissors);}
+
+};
+
+console.log('Computer\'s choice: ' + getComputerChoice());
+
