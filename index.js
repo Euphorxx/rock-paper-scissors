216393e41613da8e1d3a72eb988f64f8fd17a983
Euphorxx <97964078+Euphorxx@users.noreply.github.com>
2026-05-15 22:58:35 +0000

    feat: add const, change strings

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const rock = 'Rock';
 const paper = 'Paper';
 const scissors = 'Scissors';
+const humanScore = 0;
+const computerScore = 0;
 
 function getComputerChoice()  {
     const random = Math.floor(Math.random() * 3) + 1;
-    if (random === 1) {return (rock);
-    } else if (random === 2) {return (paper);
-    } else if (random === 3) {return (scissors);}
+    if (random === 1) {return (rock), console.log('Computer\'s choice: Rock');
+    } else if (random === 2) {return (paper), console.log('Computer\'s choice: Paper');
+    } else if (random === 3) {return (scissors), console.log('Computer\'s choice: Scissors');}
 
 };
 
 function getHumanChoice(random)  {
     random = prompt('Rock, Paper or Scissors?').toLowerCase();
-    if (random === 'rock') {return (rock);
-    } else if (random === 'paper') {return (paper);
-    } else if (random === 'scissors') {return (scissors);
+    if (random === 'rock') {return (rock), console.log('Your choice: Rock');
+    } else if (random === 'paper') {return (paper), console.log('Your choice: Paper');
+    } else if (random === 'scissors') {return (scissors), console.log('Your choice: Scissors');
     } else if (random != rock || paper || scissors) {return alert('Cancelled')};
 
 };
 
-console.log('Your choice: ' + getHumanChoice());
-console.log('Computer\'s choice: ' + getComputerChoice());
+
+
+
+
+
 
