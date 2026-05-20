bba1e140ca2628b73b1ef8fc1119ab41b3944cc0
Euphorxx <97964078+Euphorxx@users.noreply.github.com>
2026-05-20 20:11:35 +0000

    feat: add logic for a single round

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const rock = 'Rock';
 const paper = 'Paper';
 const scissors = 'Scissors';
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice()  {
     const computerSelection = Math.floor(Math.random() * 3) + 1;
@@ -21,4 +21,19 @@ function getHumanChoice(random)  {
     } else if (random != rock || paper || scissors) {return alert('Cancelled')};
 
 };
+
+humanChoice = getHumanChoice();
+computerChoice = getComputerChoice();
+
+function playRound()  {
+    if (humanChoice === computerChoice) {return alert(`${humanChoice} vs ${computerChoice} — It's a draw!`);
+    } else if (humanChoice === rock, computerChoice === paper) {return alert(`${humanChoice} vs ${computerChoice} — Computer wins!`), computerScore++;
+    } else if (humanChoice === paper, computerChoice === rock) {return alert(`${humanChoice} vs ${computerChoice} — Human wins!`), humanScore++;
+    } else if (humanChoice === rock, computerChoice === scissors) {return alert(`${humanChoice} vs ${computerChoice} — Human wins!`), humanScore++;
+    } else if (humanChoice === scissors, computerChoice === rock) {return alert(`${humanChoice} vs ${computerChoice} — Computer wins!`), computerScore++;
+    } else if (humanChoice === paper, computerChoice === scissors) {return alert(`${humanChoice} vs ${computerChoice} — Computer wins!`), computerScore++;
+    } else if (humanChoice === scissors, computerChoice === paper) {return alert(`${humanChoice} vs ${computerChoice} — Human wins!`), humanScore++;
+    } else {return alert('Cancelled')};
+
+}
 
